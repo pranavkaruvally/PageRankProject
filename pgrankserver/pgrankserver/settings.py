6b9a547d7e52882db5e4feecbcaa22649158be65
Your Name <pranavsdreams@gmail.com>
2022-08-01 22:04:15 +0530

    Fixed loading issue of pgrankserver

--- pgrankserver/pgrankserver/settings.py
+++ pgrankserver/pgrankserver/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'server.apps.ServerConfig',
-    'sites.apps.SitesConfig',
+    #'sites.apps.SitesConfig',
 
 ]
 
