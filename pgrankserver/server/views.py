6fe34046bf3bd297361074369be3297417a6e05f
Your Name <pranavsdreams@gmail.com>
2022-08-06 13:29:36 +0530

    Checked the looping method

--- pgrankserver/server/views.py
+++ pgrankserver/server/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+class Cats:
+    def __init__(self):
+        self.cathead = "So cat's are dumb huh?"
+        self.catbody = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit delectus voluptas sequi animi culpa, impedit, beatae quod optio minus saepe earum quos ut soluta magnam doloribus veniam quibusdam vero fuga."
+
 def home(request):
     return render(request, 'search.html')
 
 def result(request):
-    return render(request, 'result.html')
+    cat = Cats()
+    cat_list = [cat] * 4
+    context = {'cat_list': cat_list}
+    return render(request, 'result.html', context)
